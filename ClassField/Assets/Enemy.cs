40e4af60cf5a53b6ded4fb7065fed24c080b04b5
gamer40930 <63327386+gamer40930@users.noreply.github.com>
2020-04-09 19:08:46 +0800

    Practice_ClassField

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 怪物屬性值
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// 怪物血量
+    /// </summary>
+    [Header("血量"), Tooltip("怪物的血量"), Range(50, 500)]
+    public int HP ;
+
+    /// <summary>
+    /// 怪物魔力
+    /// </summary>
+    [Header("魔力"), Tooltip("怪物的魔力"), Range(1, 100)]
+    public int MP = 100;
+
+    /// <summary>
+    /// 怪物移動速度
+    /// </summary>
+    [Header("移動速度"), Tooltip("怪物的移動速度"), Range(1, 50.5f)]
+    public float MoveSpead = 10.5f;
+
+    /// <summary>
+    /// 怪物攻擊力
+    /// </summary>
+    [Header("攻擊力"), Tooltip("怪物的攻擊力"), Range(20, 200)]
+    public int ATK = 50;
+
+    /// <summary>
+    /// 怪物防禦力
+    /// </summary>
+    [Header("防禦力"), Tooltip("怪物的防禦力"), Range(0, 100)]
+    public int DEF ;
+
+    /// <summary>
+    /// 怪物的武器名稱
+    /// </summary>
+    [Header("武器名稱"), Tooltip("武器名稱")]
+    public string WeaponName = "";
+
+    /// <summary>
+    /// 怪物的裝備名稱
+    /// </summary>
+    [Header("裝備名稱"), Tooltip("裝備名稱")]
+    public string EquipmentName = "皮衣";
+
+    /// <summary>
+    /// 怪物是否帶有鑰匙
+    /// </summary>
+    [Header("是否帶有鑰匙"), Tooltip("怪物是否帶有鑰匙")]
+    public bool Key = false;
+
+    /// <summary>
+    /// 怪物是否掉落寶物
+    /// </summary>
+    [Header("是否掉落寶物"), Tooltip("怪物是否掉落寶物")]
+    public bool Treasure = false;
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    /// <summary>
+    /// 怪物一號
+    /// </summary>
+    [Header ("怪物一號")]
+    public Enemy Enemy01;
+
+    /// <summary>
+    /// 怪物二號
+    /// </summary>
+    [Header("怪物二號")]
+    public Enemy Enemy02;
+
+    private void Start()
+    {
+
+        //取得欄位練習//
+        print(Enemy01.MP);            //輸出怪物一號的魔力值
+        print(Enemy02.EquipmentName); //輸出怪物二號的裝備名稱
+
+        //設定欄位練習//
+
+        Enemy01.HP = 100;              //修改怪物一號的血量為 100
+        Enemy02.HP = 300;              //修改怪物二號的血量為 300
+        Enemy01.DEF = 50;              //修改怪物一號的防禦力為 50
+        Enemy01.WeaponName = "木棍";   //修改怪物一號的武器名稱為 ”木棍”
+        Enemy02.WeaponName = "短刀";   //修改怪物二號的武器名稱為 ”短刀”
+        Enemy01.Key = true;            //修改怪物一號的是否帶有鑰匙為 是
+        Enemy02.Treasure = true;       //修改怪物二號的是否帶有寶物為 是
+
+
+    }
+}
